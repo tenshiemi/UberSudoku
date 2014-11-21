689de2eed436d85e7fcb11898114e0cdc6d39d26
Emily Chen <tenshiemi@gmail.com>
2014-11-20 19:24:35 -0800

    Added code to check input

--- game.js
+++ game.js
@@ -1,12 +1,11 @@
-var isInputValid = function() {
-
+var isInputValid = function(cellInput) {
+  return !isNaN(cellInput);
 }
 
 var gameJS = function() {
   $("input").on('keyup', function() {
     var cellId = $(this).attr("data-cell");
     var cellValue = $(this).val();
-    isInputValid(cellValue);
   });
 }
 
