dbf41d97964bd008250ea350e403c30b3d16c15a
Emily Chen <tenshiemi@gmail.com>
2014-11-21 06:55:15 -0800

    Compares current board against solution board

--- game.js
+++ game.js
@@ -60,6 +60,28 @@ var isGroupValid = function(cellInput, currentGroup) {
   return true;
 }
 
+var getCurrentBoard = function() {
+  var board = getCurrentRow("0");
+  for (var i=1; i<9; i++) {
+    board += "," + getCurrentRow(i.toString());
+  }
+  return board;
+}
+
+var checkCompleteBoard = function() {
+  var solution =  "5,3,4,6,7,8,9,1,2," +
+                  "6,7,2,1,9,5,3,4,8," +
+                  "1,9,8,3,4,2,5,6,7," +
+                  "8,5,9,7,6,1,4,2,3," +
+                  "4,2,6,8,5,3,7,9,1," +
+                  "7,1,3,9,2,4,8,5,6," +
+                  "9,6,1,5,3,7,2,8,4," +
+                  "2,8,7,4,1,9,6,3,5," +
+                  "3,4,5,2,8,6,1,7,9"
+  currentBoard = getCurrentBoard();
+  return (solution === currentBoard);
+}
+
 var gameJS = function() {
   $("input").on('keyup', function() {
     var cellId = $(this).attr("data-cell");
