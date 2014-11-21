49771e7cc02d3feec9c02999ff5958041b4724d7
Emily Chen <tenshiemi@gmail.com>
2014-11-20 20:05:52 -0800

    added row validation function and test

--- game.js
+++ game.js
@@ -1,5 +1,16 @@
+var getCurrentRow = function(cellInput) {
+
+}
+
 var isInputValid = function(cellInput) {
   return !isNaN(cellInput);
+}
+
+var isRowValid = function(cellInput, currentRow) {
+  if (currentRow.indexOf(cellInput) !== -1) {
+    return false;
+  }
+  return true;
 }
 
 var gameJS = function() {
