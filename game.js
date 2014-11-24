e2a1bee89f2c3f107ab3f1890faf9ea332eeb9b7
Emily Chen <tenshiemi@gmail.com>
2014-11-24 12:52:11 -0800

    skeleton refactor

--- game.js
+++ game.js
@@ -48,18 +48,23 @@ var getCurrentBoard = function() {
   return board;
 }
 
+var checkAllRows = function() {
+
+}
+
+var checkAllColumns = function() {
+
+}
+
+var checkAllCellSquares = function() {
+
+}
+
 var checkCompleteBoard = function() {
-  var solution =  "5,3,4,6,7,8,9,1,2," +
-                  "6,7,2,1,9,5,3,4,8," +
-                  "1,9,8,3,4,2,5,6,7," +
-                  "8,5,9,7,6,1,4,2,3," +
-                  "4,2,6,8,5,3,7,9,1," +
-                  "7,1,3,9,2,4,8,5,6," +
-                  "9,6,1,5,3,7,2,8,4," +
-                  "2,8,7,4,1,9,6,3,5," +
-                  "3,4,5,2,8,6,1,7,9"
-  currentBoard = getCurrentBoard();
-  return (solution === currentBoard);
+  if (checkAllRows() && checkAllColumns() && checkAllCellSquares()) {
+    return true;
+  }
+  return false
 }
 
 var toggleWinningModal = function() {
